0776df4a9d5ac3769bb3ae885e4894d6a88228a0
Postmaxxx <71179917+Postmaxxx@users.noreply.github.com>
2021-05-16 16:00:30 +0800

    Final commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.[chunkhash].js'
     },
     
     module: {
